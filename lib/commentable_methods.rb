3f37f9b277a4684bc8b3575f0272d411503995be
yibin <ivalzheng@gmail.com>
2013-03-14 22:13:24 +0800

    add :: to Comment

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -18,21 +18,17 @@ module Juixe
 
             def self.find_comments_for(obj)
               commentable = self.base_class.name
-              Comment.find_comments_for_commentable(obj.app_code, commentable, obj.id)
+              ::DS::Comment.find_comments_for_commentable(obj.app_code, commentable, obj.id)
             end
 
             def self.find_comments_by_user(app_code, user) 
               commentable = self.base_class.name
-              Comment.joins(:app).where(tb_app: {code: app_code}).
+              ::DS::Comment.joins(:app).where(tb_app: {code: app_code}).
                 where(["user_id = ? and commentable_type = ?", user.id, commentable]).order("created_at DESC")
             end
 
             def comments_ordered_by_submitted
-              Comment.find_comments_for_commentable(app_code, self.class.name, id)
-            end
-
-            def add_comments(comment)
-              comments << comment
+              ::DS::Comment.find_comments_for_commentable(app_code, self.class.name, id)
             end
           }
         end
